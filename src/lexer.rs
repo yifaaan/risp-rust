cc967414b80662b77148703ca8031b8e4a866d30
yifaaan <0219lyf@gmail.com>
2024-08-23 18:53:59 +0800

    add tokenize and test

---
+++ src/lexer.rs
@@ -0,0 +1,101 @@
+use std::error::Error;
+
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Integer(i64),
+    Symbol(String),
+    LParen,
+    RParen,
+}
+
+#[derive(Debug)]
+pub struct TokenError {
+    ch: char,
+}
+
+impl Error for TokenError {}
+
+impl std::fmt::Display for TokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "unexpected character: {}", self.ch)
+    }
+}
+
+pub fn tokenize(program: &str) -> Result<Vec<Token>, TokenError> {
+    let program = program.replace("(", " ( ").replace(")", " ) ");
+    let words = program.split_whitespace();
+    let mut tokens = Vec::new();
+    for word in words {
+        match word {
+            "(" => tokens.push(Token::LParen),
+            ")" => tokens.push(Token::RParen),
+            _ => {
+                let maybe_int = word.parse::<i64>();
+                if maybe_int.is_ok() {
+                    tokens.push(Token::Integer(maybe_int.unwrap()));
+                } else {
+                    tokens.push(Token::Symbol(word.to_string()));
+                }
+            }
+        }
+    }
+    Ok(tokens)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let tokens = tokenize("(+ 1 2)").unwrap_or(vec![]);
+        assert_eq!(
+            tokens,
+            vec![
+                Token::LParen,
+                Token::Symbol("+".to_string()),
+                Token::Integer(1),
+                Token::Integer(2),
+                Token::RParen,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_area_of_circle() {
+        let program = "
+            (
+                (define r 10)
+                (define pi 314)
+                (* pi (* r r))
+            )
+        ";
+        let tokens = tokenize(program).unwrap_or(vec![]);
+        assert_eq!(
+            tokens,
+            vec![
+                Token::LParen,
+                Token::LParen,
+                Token::Symbol("define".to_string()),
+                Token::Symbol("r".to_string()),
+                Token::Integer(10),
+                Token::RParen,
+                Token::LParen,
+                Token::Symbol("define".to_string()),
+                Token::Symbol("pi".to_string()),
+                Token::Integer(314),
+                Token::RParen,
+                Token::LParen,
+                Token::Symbol("*".to_string()),
+                Token::Symbol("pi".to_string()),
+                Token::LParen,
+                Token::Symbol("*".to_string()),
+                Token::Symbol("r".to_string()),
+                Token::Symbol("r".to_string()),
+                Token::RParen,
+                Token::RParen,
+                Token::RParen
+            ]
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,3 @@
-use std::io;
+mod lexer;
 
-use parse::{default_env, tokenize};
-use types::{RispEnv, RispErr, RispExp};
-
-mod eval;
-mod parse;
-mod types;
-
-fn parse_eval(expr: String, env: &mut RispEnv) -> Result<RispExp, RispErr> {
-    let (parsed_exp, _) = parse::parse(&tokenize(expr))?;
-    let evaled_exp = eval::eval(&parsed_exp, env)?;
-    Ok(evaled_exp)
-}
-
-fn slurp_expr() -> String {
-    let mut expr = String::new();
-    io::stdin()
-        .read_line(&mut expr)
-        .expect("Failed to read line");
-    expr
-}
-fn main() {
-    let env = &mut default_env();
-    loop {
-        println!("risp >");
-        let expr = slurp_expr();
-        match parse_eval(expr, env) {
-            Ok(res) => println!("//🔥=> {}", res),
-            Err(e) => match e {
-                RispErr::Reason(msg) => println!("//🙀=> {}", msg),
-            },
-        }
-    }
-}
+fn main() {}
