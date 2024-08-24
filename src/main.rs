e55a39384588a646e4c9a9a30c7fb81757e37f23
yifaaan <0219lyf@gmail.com>
2024-08-24 11:43:38 +0800

    main

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,43 @@ mod eval;
 mod lexer;
 mod parser;
 
-fn main() {}
+use linefeed::{Interface, ReadResult};
+use parser::Object;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+const PROMPT: &str = "risp -> ";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let reader = Interface::new(PROMPT).unwrap();
+    let mut env = Rc::new(RefCell::new(env::Env::new()));
+
+    reader.set_prompt(format!("{}", PROMPT).as_ref()).unwrap();
+
+    while let ReadResult::Input(input) = reader.read_line().unwrap() {
+        if input.eq("exit") {
+            break;
+        }
+        let val = eval::eval(input.as_ref(), env.clone())?;
+        match val {
+            Object::Void => {}
+            Object::Integer(n) => println!("{}", n),
+            Object::Bool(b) => println!("{}", b),
+            Object::Symbol(s) => println!("{}", s),
+            Object::Lambda(params, body) => {
+                println!("Lambda(");
+                for param in params {
+                    println!("{} ", param);
+                }
+                println!(")");
+                for expr in body {
+                    println!(" {}", expr);
+                }
+            }
+            _ => println!("{}", val),
+        }
+    }
+
+    println!("Good bye");
+    Ok(())
+}
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 //! parser生成代表整个程序的一个List Object
 use crate::lexer::{tokenize, Token};
 use std::error::Error;
-
+use std::fmt;
 #[derive(Debug)]
 pub struct ParseError {
     err: String,
@@ -26,7 +26,37 @@ pub enum Object {
     Lambda(Vec<String>, Vec<Object>),
     List(Vec<Object>),
 }
-
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Object::Void => write!(f, "Void"),
+            Object::Integer(n) => write!(f, "{}", n),
+            Object::Bool(b) => write!(f, "{}", b),
+            Object::Symbol(s) => write!(f, "{}", s),
+            Object::Lambda(params, body) => {
+                write!(f, "Lambda(")?;
+                for param in params {
+                    write!(f, "{} ", param)?;
+                }
+                write!(f, ")")?;
+                for expr in body {
+                    write!(f, " {}", expr)?;
+                }
+                Ok(())
+            }
+            Object::List(list) => {
+                write!(f, "(")?;
+                for (i, obj) in list.iter().enumerate() {
+                    if i > 0 {
+                        write!(f, " ")?;
+                    }
+                    write!(f, "{}", obj)?;
+                }
+                write!(f, ")")
+            }
+        }
+    }
+}
 pub fn parse(program: &str) -> Result<Object, ParseError> {
     let tokens = tokenize(program);
     if tokens.is_err() {
